c57a103f8edcbe08a60007e0095d209a31a75fac
Ecoll69 <apd1.ecolletta@its4puntozero.it>
2026-06-30 15:55:33 +0200

    Update progetto_main.py

--- progetto (1)/progetto/progetto_main.py
+++ progetto (1)/progetto/progetto_main.py
@@ -5,11 +5,20 @@ from .progetto_db import dbinit
 from .progetto_prodotti import router as prodotti_router
 from .progetto_utente import router as utenti_router
 from .progetto_film import router as film_router
+from FastApi.middleware.cors import CORSMiddleware
 
 # Inizializzazione database all'avvio
 dbinit()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+    
 
 # Agganciamo entrambi i router all'applicazione principale (Lezione 7, Slide 5)
 app.include_router(prodotti_router)
