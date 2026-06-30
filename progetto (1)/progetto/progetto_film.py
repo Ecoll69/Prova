d17e6a01f651d6c1ccbd372c3851973ed3dd292f
Ecoll69 <apd1.ecolletta@its4puntozero.it>
2026-06-30 15:56:25 +0200

    Update progetto_film.py

--- progetto (1)/progetto/progetto_film.py
+++ progetto (1)/progetto/progetto_film.py
@@ -3,17 +3,16 @@ import sqlite3
 
 router = APIRouter()
 
-@router.get("/film/cerca")
-def cerca_film(keyword: str):
+ 
+   @router.get("/film/{id_film}")
+def dettaglio_film(id_film: int):
     conn = sqlite3.connect("database.db")
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row  # Fondamentale per leggere i dati come dizionario
     cursor = conn.cursor()
-    
-    
-    cursor.execute("SELECT * FROM film WHERE titolo LIKE ?", (f"%{keyword}%",))
-    risultato = cursor.fetchall()
+    # Query mirata usando il Path Parameter id_film passato dall'URL
+    cursor.execute("SELECT * FROM film WHERE id = ?", (id_film,))
+    risultato = cursor.fetchone()  # Recuperiamo un solo risultato (il singolo film)
     conn.close()
-    
     if not risultato:
         raise HTTPException(status_code=404, detail="Film non trovato")
     return risultato
